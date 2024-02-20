ddff97bbbbe6e25bd0b8c8d111795538f3192a41
Aditya Patel <adityapatel0905@gmail.com>
2024-02-20 02:57:38 -0500

    big changes

---
+++ PersonalWebsite/src/components/body/AboutMe.jsx
@@ -0,0 +1,13 @@
+export const AboutMe = () => {
+
+  return (
+    <>
+      <div className='text-center text-white'>
+        <h2 className="mx-10 mt-20 mb-2 text-4xl font-bold bg-gradient-to-br from-teal to-lime bg-clip-text text-transparent">About Me</h2>
+        <p className="mx-2 pb-2 text-xl">
+          I'm a <span className="font-bold">1st</span> year student at the University of Windsor.
+        </p>
+      </div>
+    </>
+  )
+}
