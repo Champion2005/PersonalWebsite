85dc5404767cc6cd8c7b28d891c7d316f7556506
Aditya Patel <adityapatel0905@gmail.com>
2024-02-20 14:40:09 -0500

    fixed nav buttons

--- PersonalWebsite/src/components/header/Navbar.jsx
+++ PersonalWebsite/src/components/header/Navbar.jsx
@@ -3,14 +3,14 @@ export const Navbar = () => {
     return (
         <>
             <div className="p-5 grid grid-cols-3 justify-evenly gap-3">
-                <button className="flex rounded-lg bg-emerald-600 ring-2 ring-emerald-400 hover:ring-offset-[3px] hover:ring-offset-base hover:bg-teal-500 hover:scale-[1.01]">
-                    <a className="py-2 grow" href='https://github.com/Champion2005' target='_blank' rel='noopener noreferrer'>Github</a>
+                <button className="flex rounded-lg bg-transparent text-emerald-400 ring-2 ring-emerald-400 hover:scale-y-110">
+                    <a className="py-2 grow hover:scale-y-90" href='https://github.com/Champion2005' target='_blank' rel='noopener noreferrer'>Github</a>
                 </button>
-                <button className="flex rounded-lg bg-emerald-600 ring-2 ring-emerald-400 hover:ring-offset-[3px] hover:ring-offset-base hover:bg-teal-500 hover:scale-[1.01]">
-                    <a className="py-2 grow" href='https://apatel.xyz/resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>
+                <button className="flex rounded-lg bg-transparent text-emerald-400 ring-2 ring-emerald-400 hover:scale-y-110">
+                    <a className="py-2 grow hover:scale-y-90" href='https://apatel.xyz/resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>
                 </button>
-                <button className="flex rounded-lg bg-emerald-600 ring-2 ring-emerald-400 hover:ring-offset-[3px] hover:ring-offset-base hover:bg-teal-500 hover:scale-[1.01]">
-                    <a className="py-2 grow" href='https://www.linkedin.com/in/aditya-patel52/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+                <button className="flex rounded-lg bg-transparent text-emerald-400 ring-2 ring-emerald-400 hover:scale-y-110">
+                    <a className="py-2 grow hover:scale-y-90" href='https://www.linkedin.com/in/aditya-patel52/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
                 </button>
             </div>
         </>
