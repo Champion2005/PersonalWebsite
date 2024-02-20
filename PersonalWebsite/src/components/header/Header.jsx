b5993149260c1959fd0f886fa6c37438e3c4b7de
Aditya Patel <adityapatel0905@gmail.com>
2024-02-20 04:47:36 -0500

    made typing animation

--- PersonalWebsite/src/components/header/Header.jsx
+++ PersonalWebsite/src/components/header/Header.jsx
@@ -1,34 +1,28 @@
-import { useState, useEffect } from 'react'
 import { Navbar } from './Navbar'
+import { TypeAnimation } from 'react-type-animation';
 
 export const Header = () => {
-  const [curPrint, setCurPrint] = useState(0);
-
-  const prints = [
-    'print("Hello World")',
-    'System.out.println("Hello World");', 
-    'println!("Hello World");', 
-    'console.log("Hello World");', 
-    'printf("%s\\n", "Hello World");',
-    'std::cout << "Hello World" << std::endl;',
-    'echo "Hello World"',
-  ]
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurPrint((prev) => (prev+1)%7);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <>
       <div className='text-center text-white'>
         <Navbar />
         <h1 className="mx-10 mb-2.5 pt-5 pb-2 text-5xl font-bold bg-gradient-to-br from-teal to-lime bg-clip-text text-transparent">Aditya Patel</h1>
-        <p className="mx-2 mb-0 mb-2 text-xl">
-          { prints[curPrint] }
+        <p className="mx-2 mb-0 mb-2 text-xl font-mono">
+          { "" }
+          <TypeAnimation
+            sequence={[
+              'print("Hello World")', 1000,
+              'std::cout << "Hello World" << std::endl;', 1000,  
+              'println!("Hello World");', 1000,
+              'console.log("Hello World");', 1000,
+              'printf("%s\\n", "Hello World");', 1000,
+              'System.out.println("Hello World");', 1000,
+              'echo "Hello World"', 1000,
+            ]}
+            speed={10}
+            repeat={Infinity}
+            showCursor={true}
+          />
         </p>
       </div>
     </>
--- PersonalWebsite/src/components/header/Navbar.jsx
+++ PersonalWebsite/src/components/header/Navbar.jsx
@@ -2,15 +2,15 @@ export const Navbar = () => {
 
     return (
         <>
-            <div className="p-5 flex justify-around gap-1 sticky top-0">
-                <button className="py-2 rounded-lg bg-red-500 hover:bg-gradient-to-r from-orange-500 to-yellow-500">
-                    <a className="py-2 px-8" href='https://github.com/Champion2005' target='_blank' rel='noopener noreferrer'>Github</a>
+            <div className="p-5 grid grid-cols-3 justify-evenly gap-3 sticky top-0">
+                <button className="flex rounded-lg bg-teal hover:bg-gradient-to-r from-orange-500 to-yellow-500">
+                    <a className="py-2 grow" href='https://github.com/Champion2005' target='_blank' rel='noopener noreferrer'>Github</a>
                 </button>
-                <button className="py-2 rounded-lg bg-red-500 hover:bg-yellow-500">
-                    <a className="py-2 px-8" href='https://apatel.xyz/resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>
+                <button className="flex rounded-lg bg-teal hover:bg-yellow-500">
+                    <a className="py-2 grow" href='https://apatel.xyz/resume.pdf' target='_blank' rel='noopener noreferrer'>Resume</a>
                 </button>
-                <button className="py-2 rounded-lg bg-red-500 hover:bg-gradient-to-r from-yellow-500 to-orange-500">
-                    <a className="py-2 mx-8" href='https://www.linkedin.com/in/aditya-patel52/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+                <button className="flex rounded-lg bg-teal hover:bg-gradient-to-r from-yellow-500 to-orange-500">
+                    <a className="py-2 grow" href='https://www.linkedin.com/in/aditya-patel52/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
                 </button>
             </div>
         </>
