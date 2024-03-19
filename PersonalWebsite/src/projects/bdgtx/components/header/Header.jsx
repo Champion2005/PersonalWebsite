c090b49c3295c32390fa3810895ea423a253f622
Champion2005 <58636533+Champion2005@users.noreply.github.com>
2024-03-18 22:53:57 -0400

    changes

--- PersonalWebsite/src/projects/bdgtx/components/header/Header.jsx
+++ PersonalWebsite/src/projects/bdgtx/components/header/Header.jsx
@@ -6,6 +6,7 @@ export const Header = () => {
             <section className='flex flex-col items-center'>
                 <div>
                     <a href="https://apatel.xyz/#/bdgtx"><img className='max-h-[10vh]' src={LOGO} alt="logo" /></a>
+                    <p className='-m-4 font-italic text-white font-bold text-lg text-center font-mono'>Control your money</p>
                 </div>
             </section>
         </>
