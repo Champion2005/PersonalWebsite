c080660cc615ec775f76b87bcc913f66766f9a6e
Champion2005 <58636533+Champion2005@users.noreply.github.com>
2024-03-18 23:00:42 -0400

    Revert "Delete PersonalWebsite/src/projects directory"
    This reverts commit 460f5e1c409990c4515cc72e37bef1332ac04cb6.

--- PersonalWebsite/src/projects/bdgtx/components/header/Header.jsx
+++ PersonalWebsite/src/projects/bdgtx/components/header/Header.jsx
@@ -6,7 +6,10 @@ export const Header = () => {
             <section className='flex flex-col items-center'>
                 <div>
                     <a href="https://apatel.xyz/#/bdgtx"><img className='max-h-[10vh]' src={LOGO} alt="logo" /></a>
+<<<<<<< HEAD
                     <p className='-m-4 font-italic text-white font-bold text-lg text-center font-mono'>Control your money</p>
+=======
+>>>>>>> parent of 460f5e1 (Delete PersonalWebsite/src/projects directory)
                 </div>
             </section>
         </>
