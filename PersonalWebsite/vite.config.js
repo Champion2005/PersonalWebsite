b9377b471d40c2616bae868ca3e3487d1581c1a9
= <=>
2024-03-21 08:35:12 -0400

    fixed vite config

--- PersonalWebsite/vite.config.js
+++ PersonalWebsite/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path";
+import process from "node:process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: "main.html",
+  },
+  root: "public",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist"
+  },
+  resolve: {
+    alias: { "/public": path.resolve(process.cwd(), "public") }
+  },
   plugins: [react()],
 })
+
